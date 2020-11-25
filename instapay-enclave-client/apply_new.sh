ead725fe4f5edf89322cd63c255d5fb75e98be3b
yoongdoo <yoongdoo0819@naver.com>
2020-11-25 13:53:49 +0900

    apply_new.sh modified

--- instapay-enclave-client/apply_new.sh
+++ instapay-enclave-client/apply_new.sh
@@ -1,10 +1,10 @@
 make clean
 make SGX_MODE=SIM
-rm -f ~/go/src/github.com/sslab-instapay/instapay-tee-client/enclave.signed.so
-rm -f ~/go/src/github.com/sslab-instapay/instapay-tee-client/enclave.so
-rm -f ~/go/src/github.com/sslab-instapay/instapay-tee-client/libtee.so
-rm -f ~/go/src/github.com/sslab-instapay/instapay-tee-client/data/key/*
-cp enclave.signed.so ~/go/src/github.com/sslab-instapay/instapay-tee-client
-cp enclave.so ~/go/src/github.com/sslab-instapay/instapay-tee-client
-cp libtee.so ~/go/src/github.com/sslab-instapay/instapay-tee-client
+rm -f ~/instapay3.0/instapay-tee-client/enclave.signed.so
+rm -f ~/instapay3.0/instapay-tee-client/enclave.so
+rm -f ~/instapay3.0/instapay-tee-client/libtee.so
+rm -f ~/instapay3.0/instapay-tee-client/data/key/*
+cp enclave.signed.so ~/instapay3.0/instapay-tee-client
+cp enclave.so ~/instapay3.0/instapay-tee-client
+cp libtee.so ~/instapay3.0/instapay-tee-client
 make clean
