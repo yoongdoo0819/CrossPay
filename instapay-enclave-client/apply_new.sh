fa3224df6824551767133488c736bce4be28d076
yoongdoo <yoongdoo0819@naver.com>
2021-03-04 12:19:04 +0900

    no key delete

--- instapay-enclave-client/apply_new.sh
+++ instapay-enclave-client/apply_new.sh
@@ -1,17 +1,17 @@
 make clean
 make SGX_MODE=SIM
-#rm -f ~/instapay3.0/instapay/src/github.com/sslab-instapay/instapay-tee-client/enclave.signed.so
-#rm -f ~/instapay3.0/instapay/src/github.com/sslab-instapay/instapay-tee-client/enclave.so
-#rm -f ~/instapay3.0/instapay/src/github.com/sslab-instapay/instapay-tee-client/libtee.so
+rm -f ~/instapay3.0/instapay/src/github.com/sslab-instapay/instapay-tee-client/enclave.signed.so
+rm -f ~/instapay3.0/instapay/src/github.com/sslab-instapay/instapay-tee-client/enclave.so
+rm -f ~/instapay3.0/instapay/src/github.com/sslab-instapay/instapay-tee-client/libtee.so
 #rm -f ~/instapay3.0/instapay/src/github.com/sslab-instapay/instapay-tee-client/data/key/*
 rm -f ~/instapay3.0/instapay-tee-client/enclave.signed.so
 rm -f ~/instapay3.0/instapay-tee-client/enclave.so
 rm -f ~/instapay3.0/instapay-tee-client/libtee.so
-rm -f ~/instapay3.0/instapay-tee-client/data/key/*
+#rm -f ~/instapay3.0/instapay-tee-client/data/key/*
 cp enclave.signed.so ~/instapay3.0/instapay-tee-client
 cp enclave.so ~/instapay3.0/instapay-tee-client
 cp libtee.so ~/instapay3.0/instapay-tee-client
-#cp enclave.signed.so ~/instapay3.0/instapay/src/github.com/sslab-instapay/instapay-tee-client/
-#cp enclave.so ~/instapay3.0/instapay/src/github.com/sslab-instapay/instapay-tee-client/
-#cp libtee.so ~/instapay3.0/instapay/src/github.com/sslab-instapay/instapay-tee-client/
+cp enclave.signed.so ~/instapay3.0/instapay/src/github.com/sslab-instapay/instapay-tee-client/
+cp enclave.so ~/instapay3.0/instapay/src/github.com/sslab-instapay/instapay-tee-client/
+cp libtee.so ~/instapay3.0/instapay/src/github.com/sslab-instapay/instapay-tee-client/
 make clean
